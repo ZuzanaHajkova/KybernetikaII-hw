5b1b64785752d4a49f11346ea70e25f2d86062f6
zuzana_h <hajkovab@vscht.cz>
2025-10-08 16:53:32 +0200

    Přidejte soubory projektu.

---
+++ semafor1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace semafor1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(52, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 63);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(52, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 63);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(52, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 63);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(391, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 63);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "spustit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(121, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 306);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ semafor1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace semafor1
+{
+    public partial class Form1 : Form
+    {
+        private int stav = 0;
+        private readonly Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 500;
+            timer.Tick += Timer_Tick;
+            
+
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                button4.Text = "start";
+            }
+            else
+            {
+                timer.Start();
+                button4.Text = "stop";
+            }
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            switch (stav)
+            
+            {
+                case 0:
+                    button1.Visible = true;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    timer.Interval = 1000;
+                    stav = 1;
+                    break;
+
+                case 1:
+                    button1.Visible = false;
+                    button2.Visible = true;
+                    button3.Visible = false;
+                    timer.Interval = 1000;
+                    stav = 2;
+                    break;
+
+                case 2:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = true;
+                    timer.Interval = 1000;
+                    stav = 3;
+                    break;
+                case 3:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    timer.Interval = 300;
+                    stav = 4;
+                    break;
+                case 4:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = true;
+                    timer.Interval = 300;
+                    stav = 5;
+                    break;
+                case 5:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    timer.Interval = 300;
+                    stav = 6;
+                    break;
+                case 6:
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = true;
+                    timer.Interval = 300;
+                    stav = 0;
+                    break;
+            }
+            
+
+        }
+    }
+}
