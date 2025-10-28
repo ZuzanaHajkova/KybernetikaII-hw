41e0c809173fb9b0e1ddd6087d53d3ce55e57538
Zuzana Hájková <zuzanahajkovaaa2@gmail.com>
2025-10-28 15:32:22 +0100

    shell

--- kyb04.sh
+++ kyb04.sh
@@ -2,11 +2,14 @@
 mkdir -p hw04/Zapis
 mkdir -p hw04/Archiv
 
+#vytvoreni souboru
 soubor="hw04/Zapis/datum_cas.txt"
 
+#vypsani datumu a casu do souboru
 echo "Datum a cas : $(date)" > "$soubor"
 
 
+#zaloha slozky
 zip -r "hw04/Archiv/backup.zip" hw04/Zapis > /dev/null
 
 echo "hotovo"
